ba1c49bfac933c94f3f91d17e2f2aa67bd627fea
Yurii <kasyurvik568@gmail.com>
2021-10-04 21:26:07 +0300

    Добавьте файлы проекта.

---
+++ ITMO21.ADO.NET/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace ITMO21.ADO.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectToDBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closreConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToDBToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connectToDBToolStripMenuItem
+            // 
+            this.connectToDBToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openConnectionToolStripMenuItem,
+            this.closreConnectionToolStripMenuItem});
+            this.connectToDBToolStripMenuItem.Name = "connectToDBToolStripMenuItem";
+            this.connectToDBToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.connectToDBToolStripMenuItem.Text = "Connect to DB";
+            // 
+            // openConnectionToolStripMenuItem
+            // 
+            this.openConnectionToolStripMenuItem.Name = "openConnectionToolStripMenuItem";
+            this.openConnectionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openConnectionToolStripMenuItem.Text = "Open connection";
+            this.openConnectionToolStripMenuItem.Click += new System.EventHandler(this.openConnectionToolStripMenuItem_Click);
+            // 
+            // closreConnectionToolStripMenuItem
+            // 
+            this.closreConnectionToolStripMenuItem.Name = "closreConnectionToolStripMenuItem";
+            this.closreConnectionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closreConnectionToolStripMenuItem.Text = "Closre connection";
+            this.closreConnectionToolStripMenuItem.Click += new System.EventHandler(this.closreConnectionToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DBConnection";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connectToDBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openConnectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closreConnectionToolStripMenuItem;
+    }
+}
+
---
+++ ITMO21.ADO.NET/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ITMO21.ADO.NET
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection = new OleDbConnection();
+        string testConnect = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;
+                             Initial Catalog=AdventureWorks2019;Data Source=DESKTOP-C3O5QE8\SQLEXPRESS";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openConnectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = testConnect;
+                    connection.Open();
+                    MessageBox.Show("Соеждинение с базой установлено");
+                }
+                else
+                {
+                    MessageBox.Show("Соединение с базой уже установлено");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка соединения с базой");
+                throw;
+            }
+        }
+
+        private void closreConnectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой закрыто");
+            }
+            else
+            {
+                MessageBox.Show("Соединение с базой уже закрыто");
+            }
+        }
+    }
+}

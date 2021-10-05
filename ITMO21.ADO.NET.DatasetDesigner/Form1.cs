a029c5a6003274b4312a57be6284215829b612e9
Yurii <kasyurvik568@gmail.com>
2021-10-05 23:38:48 +0300

    ADO.NET Labs_04_exp01 Finish

---
+++ ITMO21.ADO.NET.DatasetDesigner/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO21.ADO.NET.DatasetDesigner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetCustomersButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet northwindDataSet1 = new NorthwindDataSet();
+            NorthwindDataSetTableAdapters.CustomerTableAdapter customerTableAdapter1 = new NorthwindDataSetTableAdapters.CustomerTableAdapter();
+
+            customerTableAdapter1.Fill(northwindDataSet1.Customer);
+
+            foreach (NorthwindDataSet.CustomerRow NWCustomer in northwindDataSet1.Customer.Rows)
+            {
+                CustomersListBox.Items.Add(NWCustomer.CustomerID);
+            }
+        }
+    }
+}

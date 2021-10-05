713b392a4c4c4974a970cd41033b4da6fd65b2df
Yurii <kasyurvik568@gmail.com>
2021-10-05 22:11:15 +0300

    ADO.NET Labs_03_exp_04 Finish

--- ITMO21.ADO.NET.DBCommand/Form1.Designer.cs
+++ ITMO21.ADO.NET.DBCommand/Form1.Designer.cs
@@ -31,6 +31,13 @@ namespace ITMO21.ADO.NET.DBCommand
         {
             this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
             this.sqlCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.sqlCommand2 = new System.Data.SqlClient.SqlCommand();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.sqlCommand3 = new System.Data.SqlClient.SqlCommand();
             this.SuspendLayout();
             // 
             // sqlConnection1
@@ -41,16 +48,75 @@ namespace ITMO21.ADO.NET.DBCommand
             // 
             // sqlCommand1
             // 
+            this.sqlCommand1.CommandText = "SELECT        Name\r\nFROM            Person.ContactType";
             this.sqlCommand1.Connection = this.sqlConnection1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запрос данных";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(66, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Вызов процедуры";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // sqlCommand2
+            // 
+            this.sqlCommand2.Connection = this.sqlConnection1;
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(347, 206);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(309, 20);
+            this.ResultTextBox.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Создание таблицы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // sqlCommand3
+            // 
+            this.sqlCommand3.Connection = this.sqlConnection1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -58,6 +124,13 @@ namespace ITMO21.ADO.NET.DBCommand
 
         private System.Data.SqlClient.SqlConnection sqlConnection1;
         private System.Data.SqlClient.SqlCommand sqlCommand1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Data.SqlClient.SqlCommand sqlCommand2;
+        private System.Windows.Forms.TextBox ResultTextBox;
+        private System.Windows.Forms.Button button3;
+        private System.Data.SqlClient.SqlCommand sqlCommand3;
     }
 }
 
--- ITMO21.ADO.NET.DBCommand/Form1.cs
+++ ITMO21.ADO.NET.DBCommand/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ITMO21.ADO.NET.DBCommand
 {
@@ -15,6 +16,75 @@ namespace ITMO21.ADO.NET.DBCommand
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Text.StringBuilder result = new StringBuilder();
+
+            sqlCommand1.CommandType = CommandType.Text;
+
+            sqlConnection1.Open();
+
+            SqlDataReader reader = sqlCommand1.ExecuteReader();
+
+            bool MoreResults = false;
+
+            do
+            {
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        result.Append(reader[i].ToString() + "\t");
+                    }
+                    result.Append(Environment.NewLine);
+
+                }
+
+                MoreResults = reader.NextResult();
+            }
+            while (MoreResults);
+
+            reader.Close();
+            sqlCommand1.Connection.Close();
+
+            textBox1.Text = result.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Text.StringBuilder results = new System.Text.StringBuilder();
+            sqlCommand2.CommandType = CommandType.StoredProcedure;
+            sqlCommand2.CommandText = "uspGetBillOfMaterials";
+            sqlCommand2.Connection.Open();
+            SqlDataReader reader = sqlCommand2.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    results.Append(reader[i].ToString() + "\t");
+                }
+                results.Append(Environment.NewLine);
+            }
+            reader.Close();
+            sqlCommand2.Connection.Close();
+            ResultTextBox.Text = results.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sqlCommand3.CommandType = CommandType.Text;
+            sqlCommand3.CommandText = "CREATE TABLE SalesPersons (" +
+                   "[SalesPersonID] [int] IDENTITY(1,1) NOT NULL, " +
+                   "[FirstName] [nvarchar](50)  NULL, " +
+                   "[LastName] [nvarchar](50)  NULL)";
+            sqlCommand3.Connection.Open();
+            sqlCommand3.ExecuteNonQuery();
+            sqlCommand3.Connection.Close();
+            MessageBox.Show("Таблица SalesPersons создана");
+
         }
     }
 }
